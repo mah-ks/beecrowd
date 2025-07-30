5e85a0056ce9cce28fbf0b0a12f1cd31397e5b1f
Maria Karoline <maria_k_da-silva@estudante.sesisenai.org.br>
2025-07-30 11:33:05 -0300

    Atividade java

--- Main.java
+++ Main.java
@@ -1,25 +1,58 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-      public static int calcularMDC(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int numeroCaso = 1;
+
+        while (true) {
+            int quantidadeBolinha = entrada.nextInt();
+            int quantidadeConsulta = entrada.nextInt();
+
+            if (quantidadeBolinha == 0 && quantidadeConsulta == 0) break;
+
+            int[] bolinhas = new int[quantidadeBolinha];
+            for (int i = 0; i < quantidadeBolinha; i++) {
+                bolinhas[i] = entrada.nextInt();
+            }
+            Arrays.sort(bolinhas); 
+
+            System.out.println("CASE# " + numeroCaso + ":");
+
+            for (int i = 0; i < quantidadeConsulta; i++) {
+                int consulta = entrada.nextInt();
+                int posicao = buscaPrimeiraPosicao(bolinhas, consulta);
+
+                if (posicao != -1) {
+                    System.out.println(consulta + "  found at " + (posicao + 1));
+                } else {
+                    System.out.println(consulta + " not found");
+                }
+            }
+
+            numeroCaso++;
         }
-        return a;
+
+        entrada.close();
     }
 
-    public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
-        int N = scanner.nextInt(); 
+    public static int buscaPrimeiraPosicao(int[] vetor, int valorProcurado) {
+        int inicio = 0, fim = vetor.length - 1;
+        int resultado = -1;
 
-        for (int i = 0; i < N; i++) {
-            int F1 = scanner.nextInt();
-            int F2 = scanner.nextInt();
-            System.out.println(calcularMDC(F1, F2));
+        while (inicio <= fim) {
+            int meio = (inicio + fim) / 2;
+
+            if (vetor[meio] == valorProcurado) {
+                resultado = meio;
+                fim = meio - 1; 
+            } else if (vetor[meio] < valorProcurado) {
+                inicio = meio + 1;
+            } else {
+                fim = meio - 1;
+            }
         }
 
-        scanner.close();
+        return resultado;
     }
 }
